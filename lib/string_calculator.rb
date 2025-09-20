990d3cc9c9a76628e292ca204891316a47b53f33
1anilkohli1 <anil.kohli@maropost.com>
2025-09-20 14:22:03 +0530

    support comma-separated numbers (green)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,9 @@ class StringCalculator
   def self.add(input)
     return 0 if input.nil? || input == ''
     return input.to_i if input.match?(/\A-?\d+\z/)
+
+    tokens = input.split(',')
+    numbers = tokens.map(&:to_i)
+    numbers.sum
   end
 end
